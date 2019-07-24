a8c61b3a9455127d483838b52e8db061ee2be297
Nelson Anane <nelsonanane@Nelsons-iMac.local>
2019-07-24 11:20:09 -0400

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ hello: "how are you" });
 });
 
 const PORT = process.env.PORT || 5005;
