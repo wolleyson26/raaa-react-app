270e6d342ec3e30a9dae0b6003f152d88db48c96
Nelson Anane <nelsonanane@Nelsons-iMac.local>
2019-07-25 17:44:05 -0400

    server

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
 
-mongoose.connect(keys.mongoURI, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+try {
+  mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    reconnectTries: 100, // Never stop trying to reconnect
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0
+  });
+
+  console.log("MongoDB Connected...");
+} catch (err) {
+  console.error(err.message);
+  // Exit process with failure
+  process.exit(1);
+}
 
 const app = express();
 
