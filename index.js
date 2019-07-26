ac958bffedb6923616610c0b904c4a26e474f945
Nelson Anane <nelsonanane@Nelsons-iMac.local>
2019-07-26 10:04:47 -0400

    events deleted

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
-app.use("/api/events", require("./routes/api/events"));
 
 // Server static assets in production
 if (process.env.NODE_ENV === "production") {
